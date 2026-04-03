76f43302f0f0f8299135c30db1b144150f9d45d5
Elvis <154028285+elvis-codev@users.noreply.github.com>
2026-04-02 23:02:18 -0300

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -1,22 +1,12 @@
 import { Sequelize } from 'sequelize';
+import pg from 'pg';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
-// export const sequelize = new Sequelize(
-//   process.env.DB_NAME,
-//   process.env.DB_USER,
-//   process.env.DB_PASS,
-//   {
-//     host: process.env.DB_HOST,
-//     port: process.env.DB_PORT,
-//     dialect: 'postgres'
-//   }
-// );
-
 export const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
-  protocol: 'postgres',
+  dialectModule: pg,
   logging: false,
   dialectOptions: {
     ssl: {
