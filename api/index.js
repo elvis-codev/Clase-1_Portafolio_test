b8df4ab5ea4b1194e9bab7c47ee173f2ea017dfc
Elvis <154028285+elvis-codev@users.noreply.github.com>
2026-04-02 22:55:49 -0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,18 +1,3 @@
-import dotenv from 'dotenv';
 import app from '../app.js';
-import { sequelize } from '../src/config/db.js';
-
-dotenv.config();
-
-// Inicializar base de datos al levantar el servidor
-(async () => {
-  try {
-    await sequelize.authenticate();
-    await sequelize.sync();
-    console.log('Base de datos conectada - ok');
-  } catch (e) {
-    console.error('Error conectando a BD:', e);
-  }
-})();
 
 export default app;
